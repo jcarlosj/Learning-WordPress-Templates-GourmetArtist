63c9869067f518823950204252e8569bc5b5923d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-26 11:52:40 -0500

    Instala "UnderScores" Base Theme, para WordPress
    Aclaración:
     - Instala plantilla base en blanco para Wordpress del sitio
       https://underscores.me/

---
+++ wp-content/themes/gourmet-artist/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Gourmet_Artist
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gourmet-artist' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'gourmet-artist' ), 'WordPress' );
+			?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gourmet-artist' ), 'gourmet-artist', '<a href="http://underscores.me/">Juan Carlos Jiménez Gutiérrez</a>' );
+			?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
