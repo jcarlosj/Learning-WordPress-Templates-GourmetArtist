188be2b5b90bab000e319fb7d6f30a88219df1fd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-27 11:48:11 -0500

    Agrega comentarios 'gulpfile.js'

--- wp-content/themes/gourmet-artist/gulpfile.js
+++ wp-content/themes/gourmet-artist/gulpfile.js
@@ -1,14 +1,17 @@
+/* Importa componentes y define variables */
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 var browserSync = require( 'browser-sync' ),    /* Importa y define 'browser-sync' */
     reload      = browserSync .reload;
 
+/* Variable que define rutas de componentes de 'bower'
+ * que serán usadas por la tarea 'sass' */
 var sassPaths = [
   'bower_components/normalize.scss/sass',
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
-
+/* Define tarea de nombre 'sass' */
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
@@ -43,6 +46,11 @@ gulp .task( 'browser-sync', function() {
     );
 });
 
-gulp.task('default', ['sass', 'browser-sync' ], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+/* Define tarea que arrancará por defecto 'gulp' */
+gulp.task(
+    'default',                          /* Nombre de la tarea */
+    [ 'sass', 'browser-sync' ],         /* Array con las tareas que se ejecutarán */
+    function() {
+        gulp.watch( ['scss/**/*.scss'], ['sass'] );
+    }
+);
