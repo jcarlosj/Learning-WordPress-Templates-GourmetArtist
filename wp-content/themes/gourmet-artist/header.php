96266293211410d07e5c6dab3ecbbb7ecef8a239
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-27 18:06:50 -0500

    Agrega estilos y refactoriza la estructura para el Header
    Aclaración:
     - Se refactoriza estructura HTML (header.php)
     - Agrega estilos para el Header (_custom.scss)
     - Genera salida de archivos CSS (app.css)

--- wp-content/themes/gourmet-artist/header.php
+++ wp-content/themes/gourmet-artist/header.php
@@ -25,23 +25,29 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gourmet-artist' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<?php the_header_image_tag(); ?>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<?php if( get_header_image_tag() ) : ?>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+			<div class="header-image" style="background-image: url( '<?php header_image(); # ó echo get_header_image() ?>' )">
+
+				<div class="site-branding text-center">
+					<?php
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+					endif;
+
+					$description = get_bloginfo( 'description', 'display' );
+					if ( $description || is_customize_preview() ) : ?>
+						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+					<?php
+					endif; ?>
+				</div><!-- .site-branding -->
+
+			</div>
+		<?php endif; ?>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gourmet-artist' ); ?></button>
