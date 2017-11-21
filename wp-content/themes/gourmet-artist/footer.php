272c5ad270ecd52b0f94bf60e0dcf6211a7696ee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-11-21 11:12:18 -0500

    Agrega menú de navegación al Footer

--- wp-content/themes/gourmet-artist/footer.php
+++ wp-content/themes/gourmet-artist/footer.php
@@ -14,16 +14,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gourmet-artist' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gourmet-artist' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gourmet-artist' ), 'gourmet-artist', '<a href="http://underscores.me/">Juan Carlos Jiménez Gutiérrez</a>' );
-			?>
+		<div class="site-info row">
+			<div class="medium-4 columns">
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer-menu',
+							'menu_id'        => 'primary-menu',
+							'items_wrap'     => '<ul id="%1$s" class="%2$s vertical medium-horizontal text-center">%3$s</ul>'
+						)
+					);
+				?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/gourmet-artist/functions.php
+++ wp-content/themes/gourmet-artist/functions.php
@@ -52,6 +52,7 @@ if ( ! function_exists( 'gourmet_artist_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'gourmet-artist' ),
+			'footer-menu' => esc_html__( 'Footer Menu', 'gourmet-artist' )
 		) );
 
 		/*
